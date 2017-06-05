f8ab5c3387c3ca5f1744f3cada49c2d5fa923cb7
Sandeep Aggarwal <@smartSandeep1129@gmail.com>
2017-06-05 20:02:55 +0530

    Carthage updates

---
+++ AlertOnboarding/AlertOnboarding.h
@@ -0,0 +1,19 @@
+//
+//  AlertOnboarding.h
+//  AlertOnboarding
+//
+//  Created by Sandeep Aggarwal on 05/06/17.
+//  Copyright © 2017 Sandeep Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertOnboarding.
+FOUNDATION_EXPORT double AlertOnboardingVersionNumber;
+
+//! Project version string for AlertOnboarding.
+FOUNDATION_EXPORT const unsigned char AlertOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertOnboarding/PublicHeader.h>
+
+
